828afbd2d5afa187da0e815618e4edb703c9dee3
kimchj <wnmh123@naver.com>
2022-01-21 11:22:54 +0900

    useState practice - set function

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ function App() {
   let [contents, setContents] = useState(["서울 맛집 그것은 진짜 맛있다.", "경기도 맛집 그것은 진짜 맛있다.", "대구 맛집 그것은 진짜 맛있다."]);
   let [like, setLike] = useState(0);
 
+  function modiTitle() {
+    let newTitles = [...titles]; // ... stread operator(전개 연산자) 이유는 모든 벼수는
+    newTitles[0] = "부산 맛집 정보";
+    setTitles(newTitles);
+  };
+  
   return (
     <div className="App">
       <div className="black-nav">
         <div>BLOG</div>
       </div>
+      <button onClick={ modiTitle }>버튼</button>
       <div className="list">
         <h3> { titles[0] } <span onClick={ () => { setLike(like + 1) } }>👍</span> { like } </h3>
         <p> { contents[0] } </p>
